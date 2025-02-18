0ce7fbed56a3b9b9986ae99e6aa16923e38f64b2
victors1998 <sistemas@olpasa.olpasa.local>
2025-02-18 17:15:55 -0500

    mm

---
+++ src/main/java/com/olpasa/BackendOApplication.java
@@ -0,0 +1,13 @@
+package com.olpasa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendOApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendOApplication.class, args);
+	}
+
+}
